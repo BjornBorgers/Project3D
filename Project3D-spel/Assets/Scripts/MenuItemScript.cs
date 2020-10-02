f964a2a47fda224b3d5e07ef4b487e9003301b3d
Noah <noah.vb@telenet.be>
2020-10-02 17:17:53 +0200

    Menu V2

---
+++ Project3D-spel/Assets/Scripts/MenuItemScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MenuItemScript : MonoBehaviour
+{
+    Color hoverColor = new Color(0,125,255);
+    Color baseColor=new Color(74,72,70);
+    public Image backGround;
+    // Start is called before the first frame update
+    void Start()
+    {
+        backGround.color = baseColor;
+    }
+
+    public void Select()
+    {
+        backGround.color = hoverColor;
+    }
+
+    public void Deselect()
+    {
+        backGround.color = baseColor;
+    }
+}
