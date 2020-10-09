e1d5f16c4d0f638356e1e06b9257e0dbf1604520
Noah <noah.vb@telenet.be>
2020-10-09 15:39:23 +0200

    Pause Menu

---
+++ Project3D-spel/Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        foreach (Gamemanager.Patient patient in Gamemanager.patientList)
+        {
+            patient.PauseTimer();
+        }
+    }
+
+    // Update is called once per frame
+    public void Quit()
+    {
+        SceneManager.LoadScene("TitleScreen");
+    }
+
+    public void Continue()
+    {
+        foreach (Gamemanager.Patient patient in Gamemanager.patientList)
+        {
+            patient.RestartTimer();
+        }
+        gameObject.SetActive(false);
+    }
+}
