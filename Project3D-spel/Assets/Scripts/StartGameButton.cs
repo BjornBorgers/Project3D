d85e19bf0ae3513105839ef992945708a15df482
Loïc <loi>
2020-11-16 01:54:09 +0100

    Tutorial lvl

--- Project3D-spel/Assets/Scripts/StartGameButton.cs
+++ Project3D-spel/Assets/Scripts/StartGameButton.cs
@@ -8,11 +8,18 @@ public class StartGameButton : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        Cursor.visible = true;
+    }
+    private void Update()
+    {
         
     }
-
     public void StartGame()
     {
         SceneManager.LoadScene("SampleScene");
+    }
+    public void StartTutorial()
+    {
+        SceneManager.LoadScene("Tutorial");
     }
 }
---
+++ Project3D-spel/Assets/Tutorial/Door1.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+public class Door1 : MonoBehaviour
+{
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("hey");
+        
+        if (other.gameObject.tag == "Player")
+        {
+            SceneManager.LoadScene("TitleScreen");
+
+
+        }
+    }
+}
