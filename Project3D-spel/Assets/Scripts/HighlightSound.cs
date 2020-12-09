66ea5939d30a39aa3355437eee58baaf8e1899b7
Koen <koen.leung@hotmail.com>
2020-12-09 19:25:49 +0100

    Menu button sounds

---
+++ Project3D-spel/Assets/Scripts/HighlightSound.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;// Required when using Event data.
+
+public class HighlightSound : MonoBehaviour
+{
+    public AudioSource soundEffect;
+    public AudioClip highlightSound;
+
+    public void HoverSound()
+    {
+        soundEffect.PlayOneShot(highlightSound);
+    }
+}
