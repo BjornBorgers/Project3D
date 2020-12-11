2af10133f125c6e6a2d58441064406b1f3c69f37
Noah <noah.vb@telenet.be>
2020-12-11 16:16:32 +0100

    Sound Button

---
+++ Project3D-spel/Assets/Scripts/LisenerInfo.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LisenerInfo : MonoBehaviourSingleton<InfoForScoreScene>
+{
+    GameObject listener;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        listener = GameObject.Find("Lisener");
+    }
+
+    public void Mute()
+    {
+        if (listener.GetComponent<AudioListener>().enabled==true)
+        {
+            listener.GetComponent<AudioListener>().enabled = false;
+        }
+        else
+        {
+            listener.GetComponent<AudioListener>().enabled = true;
+        }
+    }
+}
