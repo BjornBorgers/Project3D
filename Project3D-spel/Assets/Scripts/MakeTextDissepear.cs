1b74dec1083db0e7d84e6f26a304d089eb5f3e10
Noah <noah.vb@telenet.be>
2021-01-08 08:39:12 +0100

    build

---
+++ Project3D-spel/Assets/Scripts/MakeTextDissepear.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MakeTextDissepear : MonoBehaviour
+{
+    public Text analyseText;
+    private float timeToAppear = 2f;
+    private float timeWhenDisappear;
+    // Start is called before the first frame update
+    void Start()
+    {
+        analyseText.enabled = true;
+        timeWhenDisappear = Time.time + timeToAppear;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (analyseText.enabled && (Time.time >= timeWhenDisappear))
+        {
+            analyseText.enabled = false;
+        }
+    }
+}
