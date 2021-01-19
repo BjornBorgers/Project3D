2b92138b7ff1f69d86e10b196437be9914fb731b
Koen <koen.leung@hotmail.com>
2021-01-19 18:11:53 +0100

    Some tutorial level stuff

---
+++ Project3D-spel/Assets/Scripts/TutorialSound.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TutorialSound : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject triageColor;
+
+    public GameObject arrow;
+    public float interval;
+
+    public void Start()
+    {
+        arrow.SetActive(false);
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        InvokeRepeating("FlashLabel", 0, interval);
+        arrow.SetActive(true);
+    }
+    private void OnTriggerStay(Collider other)
+    {
+        FlashLabel();
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        arrow.SetActive(false);
+    }
+    void FlashLabel()
+    {
+        if (triageColor.activeSelf)
+            triageColor.SetActive(false);
+        else
+            triageColor.SetActive(true);
+    }
+}
